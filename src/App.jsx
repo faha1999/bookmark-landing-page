d85e570d12308adc4766146365474b2c524db63c
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-09-05 22:50:16 +0600

    complete download desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Download } from './components/Download/Download';
 import { Features } from './components/Features/Features';
 import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
@@ -9,6 +10,7 @@ export const App = () => {
       <Header />
       <Hero />
       <Features />
+      <Download />
     </>
   );
 };
