a0d2971f38d38ad5b60a9193d429868a8b22f7b8
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-09-08 20:51:35 +0600

    fixed media issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ export const Header = () => {
     <>
       <nav className="navbar navbar-expand-md pt-5 pb-4">
         <div className="container">
-          <a className="navbar-brand" href="#">
+          <a className={`${isOpen ? 'd-none' : ' navbar-brand'}`} href="#">
             <img src={logo} alt="logo" />
           </a>
 
@@ -39,7 +39,7 @@ export const Header = () => {
             <button>LOGIN</button>
           </div>
 
-          <div className="togglerButton">
+          <div className={`${isOpen ? 'd-none ' : 'togglerButton'}`}>
             {isOpen ? (
               ''
             ) : (
@@ -50,40 +50,42 @@ export const Header = () => {
           </div>
 
           <div className={`${isOpen ? 'mobileNav' : 'd-none'}`}>
-            <div className="mobileLogo">
-              <div className="logo">
-                <img src={logoMobile} alt="logoMobile" />
+            <div className="d-fixed">
+              <div className="mobileLogo">
+                <div className="logo">
+                  <img src={logoMobile} alt="logoMobile" />
+                </div>
+
+                <button className="ms-auto" onClick={() => setIsOpen(false)}>
+                  <img className="close" src={close} alt="close" />
+                </button>
+              </div>
+              <ul className="navbar-nav mb-2 mb-lg-0">
+                <li className="nav-item me-4">
+                  <a className="nav-link" aria-current="page" href="#">
+                    FEATURES
+                  </a>
+                </li>
+                <li className="nav-item me-4">
+                  <a className="nav-link" href="#">
+                    PRICING
+                  </a>
+                </li>
+                <li className="nav-item me-4">
+                  <a href="#" className="nav-link">
+                    CONTACT
+                  </a>
+                </li>
+              </ul>
+
+              <div className="logIn">
+                <button>LOGIN</button>
               </div>
 
-              <button className="ms-auto" onClick={() => setIsOpen(false)}>
-                <img className="close" src={close} alt="close" />
-              </button>
-            </div>
-            <ul className="navbar-nav mb-2 mb-lg-0">
-              <li className="nav-item me-4">
-                <a className="nav-link" aria-current="page" href="#">
-                  FEATURES
-                </a>
-              </li>
-              <li className="nav-item me-4">
-                <a className="nav-link" href="#">
-                  PRICING
-                </a>
-              </li>
-              <li className="nav-item me-4">
-                <a href="#" className="nav-link">
-                  CONTACT
-                </a>
-              </li>
-            </ul>
-
-            <div className="logIn">
-              <button>LOGIN</button>
-            </div>
-
-            <div className="social-icons">
-              <img className="me-5" src={facebook} alt="facebook" />
-              <img src={twitter} alt="twitter" />
+              <div className="social-icons">
+                <img className="me-5" src={facebook} alt="facebook" />
+                <img src={twitter} alt="twitter" />
+              </div>
             </div>
           </div>
         </div>
